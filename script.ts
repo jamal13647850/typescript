e1f0d1b6de08e87aa87a57899eca7c9b511c3c42
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-17 00:26:40 +0330

    Some changes

--- script.ts
+++ script.ts
@@ -14,16 +14,95 @@ x[2]=10;
 x[3]=20;
 console.log(x);
 
+
+//object type
+const person: {
+    name: string;
+    age: number;  
+  } = {
+    name: 'Maximilian',
+    age: 30
+  };
+  console.log(person.name);
+
+//union type
+function combineun(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+      result = input1 + input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+//literal type
+function combinelt(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text'
+  ) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+      result = +input1 + +input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+    return result;
+  }  
+//alias type
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+type User = { name: string; age: number };
+
+function combineal(
+    input1: Combinable,
+    input2: Combinable,
+    resultConversion:ConversionDescriptor
+  ) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+      result = +input1 + +input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+    return result;
+  } 
+
+
+//enum type  
 enum Color {Red , Green , Yellow}
 let c : Color = Color.Green;
 let c1 : String = Color[1];
 console.log(c,c1);
 
+//any type
 let anyVar : any = 5;
 anyVar = "hesam";
 anyVar = true;
 
 let list3 : any[] = [20,"jamal",true];
+
+//function type
+const myfunc:Function=():number=>{
+    return 5;
+}
+//or
+let myfunc2: () => number;
+const add=()=>{
+    return 5;
+}
+myfunc2 = add
+
+
+
+//callback type
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+
+
 
 
 
@@ -53,9 +132,86 @@ function buildName(firstName : string , LastName?: string){
 console.log(buildName("jamal"));
 
 
+//function overload
+function add1(a: number, b: number): number;
+function add1(a: string, b: string): string;
+function add1(a: string, b: number): string;
+function add1(a: number, b: string): string;
+function add1(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add1('Max', ' Schwarz');
+result.split(' ');
+
+
+//optional chaining
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Max',
+    job: { title: 'CEO', description: 'My own company' }
+  };
+console.log(fetchedUserData?.job?.title);
+
+
+//nullish coalescing
+const userInput = '';
+
+const storedData = userInput ?? 'DEFAULT';
+
+console.log(storedData);
+
+
+//access modifiers
+class Department {
+    private employees: string[] = [];
+  
+    constructor(private id: string, public name: string) {
+  
+    }
+  
+    describe(this: Department) {
+      console.log(`Department (${this.id}): ${this.name}`);
+    }
+}
+//geter and seter
+class AccountingDepartment extends Department {
+    private lastReport: string;
+  
+    get mostRecentReport() {
+      if (this.lastReport) {
+        return this.lastReport;
+      }
+      throw new Error('No report found.');
+    }
+    set mostRecentReport(value: string) {
+        if (!value) {
+          throw new Error('Please pass in a valid value!');
+        }
+        this.lastReport=value;
+      }
+}
+const accounting = new AccountingDepartment('d2', []);
+console.log(accounting.mostRecentReport);
 
 
 
+//abstract
+abstract class Department2 {
+    static fiscalYear = 2020;
+    protected employees: string[] = [];
+  
+    constructor(protected readonly id: string, public name: string) {
+  
+    }
+    abstract describe(this: Department): void;
+
+}
+
+//interface
 interface UserInterface{
     name : string;
     email : string;
